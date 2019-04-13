f19ff6442b56b75f1cd0178a0645499f896f5b76
zen-star <luckyzsd@163.com>
2019-04-13 16:15:33 +0800

    add config.py and visualize.py

--- data/transforms/transform.py
+++ data/transforms/transform.py
@@ -3,10 +3,10 @@
 @author: Zheng Shida
 @contact: luckyzsd@163.com
 """
+
 import torch
 import torchvision.transforms as T
-import torch.nn.functional as F
-from PIL import Image
+from PIL import Image, ImageEnhance
 import numpy as np
 import random
 import math
@@ -20,8 +20,8 @@ import math
 # Note: PIL.Image: w,h;    numpy image: h,w,c;    torch image: c,h,w
 # --------------------------------------------------------------------------------------------------------------
 
-__all__ = ['Normalize', 'ToTensor', 'RandomFlip', 'RandomErasing', 'Random90Rotate',
-           'Resize']
+__all__ = ['Normalize', 'ToTensor', 'RandomFlip', 'Random90Rotate', 'Resize', 'Pad', 'RandomCrop',
+           'RandomErasing', 'ColorJitter']
 
 
 class Normalize(object):
@@ -81,7 +81,7 @@ class RandomFlip(object):
         inst = sample['inst_label']
 
         if random.random() < self.prob:
-            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+            img = img.transpose(self.method)
             mask = Image.fromarray(mask.astype(np.uint8), mode='L').transpose(self.method)
             mask = np.array(mask)
             for i in range(len(inst)):
@@ -144,10 +144,7 @@ class Resize(object):
         mask = np.array(mask)
         inst2 = []
         for i in range(len(inst)):
-            def _resize_np(instx):
-                np.array(self.resize(Image.fromarray(instx.astype(np.uint8), mode='L')))
-                return instx
-            inst2.append(_resize_np(inst[i]))
+            inst2.append(np.array(self.resize(Image.fromarray(inst[i].astype(np.uint8), mode='L'))))
         return {
             'image': img,
             'seg_label': mask,
@@ -178,15 +175,46 @@ class Pad(object):
 
 
 class RandomCrop(object):
-    pass
+    def __init__(self, size):
+        self.size = size
+
+    @staticmethod
+    def get_params(img, output_size):
+        """
+        get crop center and w/h
+        :param img: PIL.Image
+        :param output_size: (h, w)
+        :return: i, j, h, w
+        """
+        w, h = img.size
+        th, tw = output_size
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return i, j, th, tw
+
+    def __call__(self, sample):
+        img = sample['image']  # PIL.Image
+        mask = sample['seg_label']  # numpy.arrays(uint8)
+        inst = sample['inst_label']
+
+        i, j, h, w = self.get_params(img, self.size)
+
+        img = img.crop((i, j, i+h, j+w))
+        mask = mask[j:j+w, i:i+h]
+        for ii in range(len(inst)):
+            inst[ii] = inst[ii][j:j+w, i:i+h]
+        return {
+            'image': img,
+            'seg_label': mask,
+            'inst_label': inst
+        }
 
 
 class RandomResizeCrop(object):
-    pass
-
-
-class ColorJitter(object):
-    pass
+    raise NotImplementedError
 
 
 class RandomErasing(object):
@@ -200,7 +228,7 @@ class RandomErasing(object):
          r1: Minimum aspect ratio of erased area.
          mean: Erasing value.
     """
-    def __init__(self, probability=0.5, sl=0.02, sh=0.4, r1=0.2, mean=(0.4914, 0.4822, 0.4465)):
+    def __init__(self, probability=0.5, sl=0.02, sh=0.2, r1=0.3, mean=(0.4914, 0.4822, 0.4465)):
         self.probability = probability
         self.mean = [int(round(i*255)) for i in mean]
         self.sl = sl
@@ -215,7 +243,7 @@ class RandomErasing(object):
             return sample
         img = np.array(img).transpose((2, 0, 1))  # w,h -> h,w,c -> c,h,w
         for attempt in range(100):
-            area = img.shape[1] * img.shape[2]
+            area = img.shape[1] * img.shape[2]  # h,w
 
             target_area = random.uniform(self.sl, self.sh) * area
             aspect_ratio = random.uniform(self.r1, 1 / self.r1)
@@ -228,7 +256,7 @@ class RandomErasing(object):
                 y1 = random.randint(0, img.shape[2] - w)
                 mask[x1:x1 + h, y1:y1 + w] = 0.
                 for i in range(len(inst)):
-                    inst[i, x1:x1 + h, y1:y1 + w] = 0.
+                    inst[i][x1:x1 + h, y1:y1 + w] = 0.
                 if img.shape[0] == 3:
                     img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
                     img[1, x1:x1 + h, y1:y1 + w] = self.mean[1]
@@ -245,65 +273,117 @@ class RandomErasing(object):
         return sample
 
 
+class ColorJitter(object):
+    """Randomly change the brightness, contrast and saturation of an image.
+
+    Args:
+        brightness (float or tuple of float (min, max)): How much to jitter brightness.
+            brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
+            or the given [min, max]. Should be non negative numbers.
+        contrast (float or tuple of float (min, max)): How much to jitter contrast.
+            contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
+            or the given [min, max]. Should be non negative numbers.
+        saturation (float or tuple of float (min, max)): How much to jitter saturation.
+            saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
+            or the given [min, max]. Should be non negative numbers.
+    """
+    def __init__(self, brightness=0, contrast=0, saturation=0):
+        self.brightness = self._check_input(brightness, 'brightness')
+        self.contrast = self._check_input(contrast, 'contrast')
+        self.saturation = self._check_input(saturation, 'saturation')
+
+    @staticmethod
+    def _check_input(value, name, center=1, bound=(0, float('inf')), clip_first_on_zero=True):
+        if isinstance(value, (float, int)):
+            if value < 0:
+                raise ValueError("If {} is a single number, it must be non negative.".format(name))
+            value = [center - value, center + value]
+            if clip_first_on_zero:
+                value[0] = max(value[0], 0)
+        elif isinstance(value, (tuple, list)) and len(value) == 2:
+            if not bound[0] <= value[0] <= value[1] <= bound[1]:
+                raise ValueError("{} values should be between {}".format(name, bound))
+        else:
+            raise TypeError("{} should be a single number or a list/tuple with lenght 2.".format(name))
+
+        # if value is 0 or (1., 1.) for brightness/contrast/saturation or (0., 0.) for hue, do nothing
+        if value[0] == value[1] == center:
+            value = None
+        return value
+
+    @staticmethod
+    def _do_jitter(img, Brightness, Contrast, Color):  # color denotes saturation
+        var_dict = dict(locals())  # locals() is python in-build function to obtain 'var':value dynamically
+        trans = []
+        for var in var_dict:
+            if len(var) > 3:
+                trans.append(var)
+        # trans = ['Brightness', 'Contrast', 'Color']
+
+        random.shuffle(trans)
+        for one in trans:
+            if one is not None:
+                one_factor = random.uniform(getattr(one, ))
+                enhancer = getattr(ImageEnhance, one)(img)
+                img = enhancer.enhance(one_factor)
+
+        return img
+
+    def __call__(self, img):
+        img = sample['image']  # PIL.Image
+        mask = sample['seg_label']  # numpy.arrays(uint8)
+        inst = sample['inst_label']
+        img = self._do_jitter(img, self.brightness, self.contrast, self.saturation)
+        return {
+            'image': img,
+            'seg_label': mask,
+            'inst_label': inst
+        }
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+        format_string += 'brightness={0}'.format(self.brightness)
+        format_string += ', contrast={0}'.format(self.contrast)
+        format_string += ', saturation={0}'.format(self.saturation)
+        return format_string
+
+
 if __name__ == '__main__':
 
-    test_id_list = [5]  # according to __all__
-
+    # selectively comment not-for-test methods
+    transform_test = T.Compose([
+        # Resize(1024),
+        RandomFlip(0.8, Image.FLIP_LEFT_RIGHT),
+        RandomFlip(0.8, Image.FLIP_TOP_BOTTOM),
+        ColorJitter(brightness=10, contrast=10, saturation=10),
+        Random90Rotate(0.8, 0.8),
+        RandomErasing(probability=0.8),
+        Pad(50),
+        RandomCrop((512, 512)),
+    ])
+    # prepare test sample: img, mask, insts
     test_img = Image.open('../../test_images/ycy.jpg')
     mask = test_img.getchannel(0).point(lambda i: i < 200 and 255)
     # mask.show(title='seg_before')
     mask = np.array(mask)
-    # Image.fromarray(mask, mode='L').show()
     mask1 = test_img.getchannel(0).point(lambda i: i < 100 and 255)
     # mask1.show(title='inst1_before')
     mask1 = np.array(mask1)
     mask2 = test_img.getchannel(0).point(lambda i: 100 <= i < 200 and 255)
     # mask2.show(title='inst2_before')
     mask2 = np.array(mask2)
-    # insts = np.concatenate((np.expand_dims(mask1, 0), np.expand_dims(mask2, 0)), 0)
     insts = [mask1, mask2]
     sample = {
         'image': test_img,
         'seg_label': mask,
         'inst_label': insts
     }
+    print(test_img.size)
+    print(mask.shape)
+    print(insts[0].shape)
 
-    if 3 in test_id_list:
-        # test for random erase
-        test_random_erase = RandomErasing(probability=0.9)
-        after_sample = test_random_erase(sample)
-        after_sample['image'].show(title='img_after')
-        Image.fromarray(after_sample['seg_label'], mode='L').point(lambda i: i < 1 and 255).show(title='seg_after')
-        Image.fromarray(after_sample['inst_label'][0], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-        Image.fromarray(after_sample['inst_label'][1], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-    else:
-        after_sample = sample
-
-    if 4 in test_id_list:
-        # test for random horizon flip
-        test_random_vflip = RandomFlip(0.9, Image.FLIP_TOP_BOTTOM)
-        after_sample = test_random_vflip(after_sample)
-        after_sample['image'].show(title='img_after')
-        Image.fromarray(after_sample['seg_label'], mode='L').point(lambda i: i < 1 and 255).show(title='seg_after')
-        Image.fromarray(after_sample['inst_label'][0], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-        Image.fromarray(after_sample['inst_label'][1], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-
-    if 5 in test_id_list:
-        # test for random horizon flip
-        test_random_90 = Random90Rotate(0.9)
-        after_sample = test_random_90(after_sample)
-        after_sample['image'].show(title='img_after')
-        Image.fromarray(after_sample['seg_label'], mode='L').point(lambda i: i < 1 and 255).show(title='seg_after')
-        Image.fromarray(after_sample['inst_label'][0], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-        Image.fromarray(after_sample['inst_label'][1], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-
-    if 6 in test_id_list:
-        # test for Resize
-        test_resize = Resize(512)
-        print(after_sample['image'].size)
-        after_sample = test_resize(after_sample)
-        print(after_sample['image'].size)
-        after_sample['image'].show(title='img_after')
-        Image.fromarray(after_sample['seg_label'], mode='L').point(lambda i: i < 1 and 255).show(title='seg_after')
-        Image.fromarray(after_sample['inst_label'][0], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
-        Image.fromarray(after_sample['inst_label'][1], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
+    after_sample = transform_test(sample)
+    after_sample['image'].show(title='img_after')
+    Image.fromarray(after_sample['seg_label'], mode='L').point(lambda i: i < 1 and 255).show(title='seg_after')
+    Image.fromarray(after_sample['inst_label'][0], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
+    Image.fromarray(after_sample['inst_label'][1], mode='L').point(lambda i: i < 1 and 255).show(title='inst_after')
